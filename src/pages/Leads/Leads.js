79859f143b673702f076e785aa0709a7852fbaa2
rasedul99 <rasedul2199@gmail.com>
2023-02-18 22:03:34 +0600

    searcbar added in lead page

--- src/pages/Leads/Leads.js
+++ src/pages/Leads/Leads.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgProfile } from "react-icons/cg";
+import { CiSearch } from "react-icons/ci";
 import { MdOutlineFullscreen } from "react-icons/md";
 
 const Leads = () => {
+  const [leads, setLeads] = useState("");
   return (
-    <div>
-      <div className="flex justify-between items-center mx-3">
+    <div className="mx-3">
+      <div className="flex justify-between items-center">
         <div>
           <p>Leads</p>
           <p>Difficulties increase the nearer we get to the goal</p>
@@ -15,8 +17,18 @@ const Leads = () => {
           <CgProfile />
         </div>
       </div>
-      <div>
-        
+      <div className="bg-slate-200 p-2 my-2">
+        <div className="flex justify-between max-w-xs rounded   p-1  border border-slate-300">
+          <input
+            placeholder="Search in leads table...."
+            className="outline-none bg-slate-200"
+            value={leads}
+            onChange={(e) => setLeads(e.target.value)}
+          />
+          <span>
+            <CiSearch size="20" />
+          </span>
+        </div>
       </div>
     </div>
   );
